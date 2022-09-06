23888208b58a85e483cdcced61ef164e6c22603c
Shweta Kanekar <kanekarshweta10@gmail.com>
2022-09-06 20:59:25 +0530

    Connected to MySQL DB using SQLAlchemy

--- app.py
+++ app.py
@@ -1,64 +1,107 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow import fields
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://master:@localhost:3306/patient'
+db = SQLAlchemy(app)
 
 
-patients = [
-    {'id': 1, 'name': 'John', 'age': 40},
-    {'id': 2, 'name': 'Tom', 'age': 45},
-    {'id': 3, 'name': 'Jerry', 'age': 28},
-    {'id': 4, 'name': 'Alice', 'age': 36},
-]
+# Patient model
+class PatientModel(db.Model):
+    __tablename__ = "patient"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return self.id
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __repr__(self):
+        return f"Patient(name = {self.name}, age = {self.age})"
+
+
+# db.create_all()
+
+
+# Patient schema
+class PatientSchema(SQLAlchemyAutoSchema):
+    class Meta(SQLAlchemyAutoSchema.Meta):
+        model = PatientModel
+        sqla_session = db.session
+    id = fields.Integer(dump_only=True)
+    name = fields.String(required=True)
+    age = fields.Integer(required=True)
 
 
 # get all patients
 @app.route('/patients', methods=['GET'])
 def getAllPatients():
-    return jsonify(patients)
+    patients = PatientModel.query.all()
+    patient_schema = PatientSchema(many=True)
+    result = patient_schema.dump(patients)
+    return result
 
 
 # get a patient by id
 @app.route('/patients/<id>', methods=['GET'])
 def getPatientById(id):
-    for i in range(len(patients)):
-        if patients[i]['id'] == int(id):
-            return jsonify(patients[i]), 200
+    patient = PatientModel.query.get(id)
+    if patient:
+        patient_schema = PatientSchema()
+        result = patient_schema.dump(patient)
+        return result
     return jsonify({'message': 'Patient not found'}), 404
 
 
 # create new patient
 @app.route('/patients', methods=['POST'])
 def createPatient():
-    patientData = request.get_json()
-    print(patientData)
-    if patientData and 'name' in patientData and 'age' in patientData:
-        patientData['id'] = int(patients[-1]['id']) + 1
-        patients.append(patientData)
-        return patientData, 201
+    data = request.get_json()
+    if data and 'name' in data and 'age' in data:
+        patient_schema = PatientSchema()
+        patient = PatientModel(data['name'], data['age'])
+        created_patient = patient_schema.dump(patient.create())
+        return created_patient, 201
     return jsonify({'message': 'name and age is required'}), 400
 
 
 # update patient
 @app.route('/patients/<id>', methods=['PUT'])
 def updatePatient(id):
-    for i in range(len(patients)):
-        if patients[i]['id'] == int(id):
-            patientData = request.get_json()
-            if patientData and 'name' in patientData and 'age' in patientData:
-                patients[i]['name'] = patientData['name']
-                patients[i]['age'] = patientData['age']
-                return jsonify(patients[i]), 200
-            return jsonify({'message': 'name and age is required'}), 400
-    return jsonify({'message': 'Patient not found'}), 404
+    data = request.get_json()
+    patient = PatientModel.query.get(id)
+    if patient is None:
+        return jsonify({'message': 'Patient not found'}), 404
+    if data.get('name'):
+        patient.name = data['name']
+    if data.get('age'):
+        patient.age = data['age']
+    patient_schema = PatientSchema()
+    updated_patient = patient_schema.dump(patient.create())
+    return updated_patient
 
 
 # delete patient
 @app.route('/patients/<id>', methods=['DELETE'])
 def deletePatient(id):
-    for i in range(len(patients)):
-        if patients[i]['id'] == int(id):
-            patients.remove(patients[i])
-            return jsonify({'message': f'Patient with id={id} is deleted.'}), 200
-    return jsonify({'message': 'Patient not found'}), 404
+    patient = PatientModel.query.get(id)
+    if patient is None:
+        return jsonify({'message': 'Patient not found'}), 404
+    patient.delete()
+    return jsonify({'message': f'Patient with id={id} is deleted.'})
 
 
 app.run(debug=True)
